7c2092a9896288e6c2548d0965844d677af4f8c8
Ishai Masada <imasada@vtcap.org>
2021-09-07 17:32:26 -0400

    Did problems 1 - 7 for week 1

---
+++ wk1.py
@@ -0,0 +1,26 @@
+1. quanitity = 5
+
+2. greeting = 'Top o\'the Mornin\''
+
+3. 
+    first = input('Input grade 1: ')
+    second = input('Input grade 2: ')
+    third = input('Input grade 3: ')
+    average = (first+second+third)/2
+    print(average)
+
+4.
+    hours = input("Type in the current hour: ")
+    minutes = input("Type in the minutes: ")
+    time = str(int(hours)) + ";" + str(int(minutes))
+    print(time)
+
+5. Your age is32and your weight is187
+
+6. 66
+
+7. 
+    myName = "Fred"
+    myLastName = "Jones"
+    print(f"My name is {myName} {myLastName} and I am a good student.")
+
