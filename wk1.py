3e4d0a21855c40825d743ffb792c43a9bd9bb6ef
Ishai Masada <imasada@vtcap.org>
2021-09-22 12:59:03 -0400

    Corrected math and syntax errors

--- wk1.py
+++ wk1.py
@@ -1,4 +1,4 @@
-1. quanitity = 5
+1. quanitity = 5.0
 
 2. greeting = 'Top o\'the Mornin\''
 
@@ -6,13 +6,13 @@
     first = input('Input grade 1: ')
     second = input('Input grade 2: ')
     third = input('Input grade 3: ')
-    average = (first+second+third)/2
+    average = (first+second+third)/3
     print(average)
 
 4.
-    hours = input("Type in the current hour: ")
-    minutes = input("Type in the minutes: ")
-    time = str(int(hours)) + ";" + str(int(minutes))
+    hours = str(input("Type in the hour of day: ")))
+    minutes = str(input("Type in the minutes: ")))
+    time = hours + ":" + minutes
     print(time)
 
 5. Your age is32and your weight is187
