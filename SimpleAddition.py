bbf8bf3c258941199770fe70dcad1c891060848c
Ishai Masada <imasada@vtcap.org>
2021-09-22 12:59:42 -0400

    Wrote the main function to add numbers

---
+++ SimpleAddition.py
@@ -0,0 +1,11 @@
+## Simple Addition
+## Ishai Masada
+## This program calculates the sum of 2 numbers
+
+def main():
+    num1 = 7
+    num2 = 5
+    answer = num1 + num2
+    print ("Sum = "+str(answer))
+
+main()
