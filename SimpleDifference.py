b7650a51e9d6849227c7a2a30b5345870817b481
Ishai Masada <imasada@vtcap.org>
2021-09-22 13:08:10 -0400

    Wrote the diff function to calculate a difference

---
+++ SimpleDifference.py
@@ -0,0 +1,11 @@
+## Simple Difference
+## Ishai Masada
+## This program calculates the difference of two numbers
+
+def diff():
+    minuend = int(input('Type the minued: '))
+    subtrahend = int(input('Type the subtrahend: '))
+    difference = minuend - subtrahend
+    print(difference)
+
+diff()
