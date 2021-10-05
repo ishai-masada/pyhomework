abaaf8f081f7c744d302dc03c2b8f6d31ac0b5e3
Ishai Masada <imasada@vtcap.org>
2021-10-05 09:29:45 -0400

    Used loops instead of copy pasting
    The extra sentences, names, etc were just copied code. Put them into loops to do away with that.

--- Lab04.py
+++ Lab04.py
@@ -1,122 +1,109 @@
 ## Python Homework 9/28/21
+## Ishai Masada
+## Lab 04/05
 
-def name_1():
-    name_1 = input("Type a name in \'first last\' order: ")
-    name_1 = name_1.split()
-    name_1.reverse()
-    print(", ".join(name_1))
-
-def name_2():
-    name_2 = input("Type a name in \'first last\' order: ")
-    name_2 = name_2.split()
-    name_2.reverse()
-    print(", ".join(name_2))
-
-
-def name_3():
-    name_3 = input("Type a name in \'first last\' order: ")
-    name_3 = name_3.split()
-    name_3.reverse()
-    print(", ".join(name_3))
+def name_reverse(name):
+    name = name.split()
+    name.reverse()
+    print(", ".join(name))
     
 def nameReverse():
-    num_names = int(input("How many names do you want to enter? "))
+    num_names = int(input("Type in the number of names you want to be reversed: "))
+    print(f'num names: {num_names}')
+    count = 0
     if num_names == 1:
-        name_1()
+        while count < 1:
+            name = input("Type in the name in first-last order: ")
+            name_reverse(name)
+            count+=1
     elif num_names == 2:
-        name_1()
-        name_2()
+        while count < 2:
+            name = input("Type in the name in first-last order: ")
+            name_reverse(name)
+            count+=1
     elif num_names == 3:
-        name_1()
-        name_2()
-        name_3()
-
-def company_1():
-    company_1 = input("Type a company url with the company name starting with a capital letter: ")
-    return company_1
-
-def company_2():
-    company_2 = input("Type a company url with the company name starting with a capital letter: ")
-    return company_2
-
-def company_3():
-    company_3 = input("Type a company url with the company name starting with a capital letter: ")
-    return company_3
+        while count < 3:
+            name = input("Type in the name in first-last order: ")
+            name_reverse(name)
+            count+=1
 
 def company_strip(company):
     company = company.lstrip("www.").rstrip(".com")
     print(company)
 
-
 def companyName():
-    num_companies = int(input("How many company urls do you want to enter? "))
-    if num_companies == 1:
-        company_1()
-        company_strip(company_1)
-    elif num_companies == 2:
-        company_1()
-        company_strip(company_1)
-        company_2()
-        company_strip(company_2)
-    elif num_companies == 3:
-        company_1()
-        company_strip(company_1)
-        company_2()
-        company_strip(company_2)
-        company_3()
-        company_strip(company_3)
-
-
-
-def initials():
-    num_names = int(input("Enter in how many names you want: "))
-    names = []
-    dump = []
-    initial_pool = []
+    num_company = int(input("How many company urls do you want to enter? "))
     count = 0
-    while count < num_names:
-        name = input("Enter the name in first-last format: ")   
-        names.append(name)
-        count += 1
-    for name in names:
-        name = name.split()
-        for word in name:
-            initial = word[0]
-            dump.append(initial) 
-            if len(name) == (name.index(word)+1):
-                initial_pool.append(''.join(dump))
-                dump = []
-    print(', '.join(initial_pool))
+    if num_company == 1:
+        while count < 1:
+            company = input("Type in the name of the company website: ")
+            company_strip(company)
+            count+=1
+    elif num_company == 2:
+        while count < 2:
+            company = input("Type in the name of the company website: ")
+            company_strip(company)
+            count+=1
+    elif num_company == 3:
+        while count < 3:
+            company = input("Type in the name of the company website: ")
+            company_strip(company)
+            count+=1
+
+
+def initial(first_name, last_name):
+    initials = []
+    for letter in first_name:
+        initials.append(letter)
+        break
+    for letter in last_name:
+        initials.append(letter)
+        break
+    return initials
+
+def find_initials():
+    num_names = int(input("Enter in how many names you want: "))
+    count = 0
+    if num_names == 1:
+        while count < 1:
+            first_name = input("Type in the first name of the student: ")
+            last_name = input("Type in the last name of the student: ")
+            initials = initial(first_name, last_name)
+            print(''.join(initials))
+            count+=1
+    elif num_names == 2:
+        while count < 2:
+            first_name = input("Type in the first name of the student: ")
+            last_name = input("Type in the last name of the student: ")
+            initials = initial(first_name, last_name)
+            print(''.join(initials))
+            count+=1
+    elif num_names == 3:
+        while count < 3:
+            first_name = input("Type in the first name of the student: ")
+            last_name = input("Type in the last name of the student: ")
+            initials = initial(first_name, last_name)
+            print(''.join(initials))
+            count+=1
 
 def wordCount():
     num_sentences = int(input("Type in the number of sentences you want: "))
+    count = 0
     if num_sentences == 1:
-        sentence_1()
-        print(f'Number of words in sentence #1: {len(sentence_1.split())}')
+        while count < 1:
+            sentence = input("Type in your sentence here: ")
+            print(f'Number of words in the sentence: {len(sentence.split())}')
+            count+=1
     elif num_sentences == 2:
-        sentence_1()
-        print(f'Number of words in sentence #1: {len(sentence_1.split())}')
-        sentence_2()
-        print(f'Number of words in sentence #2: {len(sentence_2.split())}')
+        while count < 2:
+            sentence = input("Type in your sentence here: ")
+            print(f'Number of words in the sentence: {len(sentence.split())}')
+            count+=1
     elif num_sentences == 3:
-        sentence_1()
-        print(f'Number of words in sentence #1: {len(sentence_1.split())}')
-        sentence_2()
-        print(f'Number of words in sentence #2: {len(sentence_2.split())}')
-        sentence_3()
-        print(f'Number of words in sentence #3: {len(sentence_3.split())}')
-
-def sentence_1():
-    sentence_1 = input("Type in your sentence here: ")
-    return sentence_1
-
-def sentence_2():
-    sentence_2 = input("Type in your sentence here: ")
-    return sentence_2
-
-def sentence_3():
-    sentence_3 = input("Type in your sentence here: ")
-    return sentence_3
+        while count < 3:
+            sentence = input("Type in your sentence here: ")
+            print(f'Number of words in the sentence: {len(sentence.split())}')
+            count+=1
 
 def average(sentence):
     lengths = []
@@ -129,20 +116,27 @@ def average(sentence):
 
 def wordAverage():
     num_sentences = int(input("Type in the number of sentences you want: "))
+    count = 0
     if num_sentences == 1:
-        average(sentence_1())
+        while count < 1:
+            sentence = input("Type in your sentence here: ")
+            average(sentence)
+            count+=1
     elif num_sentences == 2:
-        average(sentence_1())
-        average(sentence_2())
+        while count < 2:
+            sentence = input("Type in your sentence here: ")
+            average(sentence)
+            count+=1
     elif num_sentences == 3:
-        average(sentence_1())
-        average(sentence_2())
-        average(sentence_3())
+        while count < 3:
+            sentence = input("Type in your sentence here: ")
+            average(sentence)
+            count+=1
 
 
 
 # nameReverse()
 # companyName()
-# initials()
+# find_initials()
 # wordCount()
 # wordAverage()
